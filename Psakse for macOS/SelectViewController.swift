a61c7d605463dafef6f8a6cfef42cea476e6e11d
Daniel Marriner <danielmarriner@anachronistic-tech.co.uk>
2019-12-08 15:01:55 +0000

    macOS SelectViewController now implemented with IB UI

--- Psakse for macOS/SelectViewController.swift
+++ Psakse for macOS/SelectViewController.swift
@@ -1,6 +1,6 @@
 //
 //  SelectViewController.swift
-//  Psakse-2
+//  Psakse
 //
 //  Created by Daniel Marriner on 10/01/2019.
 //  Copyright © 2019 Daniel Marriner. All rights reserved.
@@ -11,31 +11,51 @@ import AppKit
 class SelectViewController: NSViewController, NSTableViewDelegate, NSTableViewDataSource {
     
     var puzzleSelected = -1
-    var table: NSTableView? = nil
     var tableData = [Puzzle]()
+    @IBOutlet weak var challengeView: NSTableView!
+    @IBOutlet weak var homeView: NSButton!
+    
+    
+    var table: NSTableView? = nil
     var override: Bool = false
     var width = 0
     var height = 0
     
-    func tableView(_ tableView: NSTableView, numberOfRowsInSection section: Int) -> Int {
+    func setupButtonView(button: NSButton, title: String, color: Colors, action: Selector) {
+        let layer = CALayer()
+        layer.backgroundColor = color.getColor().cgColor
+        let text = CATextLayer()
+        text.string = title
+        text.frame = CGRect(x: 0, y: button.bounds.height / 2.7, width: button.bounds.width, height: button.bounds.height)
+        let CGR = NSClickGestureRecognizer(target: self, action: action)
+        button.addGestureRecognizer(CGR)
+        text.foregroundColor = NSColor.darkGray.cgColor
+        text.fontSize = 16
+        text.alignmentMode = .center
+        layer.addSublayer(text)
+        layer.cornerRadius = button.frame.height / 2
+        button.layer = layer
+        button.setBorder(width: 3, color: .darkGray)
+    }
+    
+    func numberOfRows(in tableView: NSTableView) -> Int {
         return tableData.count
     }
     
-//    func tableView(_ tableView: NSTableView, cellForRowAt indexPath: IndexPath) -> NSTableCellView {
-//        let cell = table?.makeView(withIdentifier: "cell", owner: nil) as! NSTableCellView {
-//            cell.textField?.stringValue
-//        }
-////        let cell = NSTableCellView(style: NSTableCellView.CellStyle.default, reuseIdentifier: "Cell")
-//        let solved = UserDefaults.standard.bool(forKey: "\(tableData[indexPath.item].id)")
-//        cell.textLabel?.text = "Puzzle \(indexPath.row + 1)" + (solved ? " - Solved" : "")
-//        return cell
-//    }
+    func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
+        let cell = challengeView.makeView(withIdentifier: NSUserInterfaceItemIdentifier(rawValue: "Cell"), owner: nil) as? NSTableCellView
+        let solved = UserDefaults.standard.bool(forKey: "\(tableData[row].id)")
+        cell?.textField?.stringValue = "Puzzle \(row + 1)" + (solved ? " - Solved" : "")
+        return cell
+    }
     
-    func tableView(_ tableView: NSTableView, didSelectRowAt indexPath: IndexPath) {
-        puzzleSelected = indexPath.item
-        for _ in 0..<tableData.count {
-            performSegue(withIdentifier: "ToScriptedPuzzle", sender: self)
+    @objc func doubleClick(_ sender: AnyObject) {
+        guard challengeView.selectedRow >= 0 else {
+            return
         }
+        puzzleSelected = challengeView.selectedRow
+        performSegue(withIdentifier: "ToScriptedPuzzle", sender: self)
+        self.view.window?.windowController?.close()
     }
     
     @objc func goToHome() {
@@ -62,7 +82,7 @@ class SelectViewController: NSViewController, NSTableViewDelegate, NSTableViewDa
                         return Int(lhs.numID)! < Int(rhs.numID)!
                     }
                     DispatchQueue.main.sync(execute: {
-                        self.table!.reloadData()
+                        self.challengeView.reloadData()
                     })
                 }
             }
@@ -71,48 +91,27 @@ class SelectViewController: NSViewController, NSTableViewDelegate, NSTableViewDa
     }
     
     override func viewDidLoad() {
-        let width = 500
-        let height = 750
-        self.view = FlippedView()
-        self.view.window?.setFrame(NSRect(x: 0, y: 0, width: width, height: height), display: true)
-        var layer = CALayer()
-        layer.backgroundColor = NSColor.white.cgColor
-        self.view.layer = layer
-        self.view.setFrameSize(NSSize(width: width, height: height))
-        self.width = width
-        self.height = height
         
         // Clear all stored data
-        //        let domain = Bundle.main.bundleIdentifier!
-        //        UserDefaults.standard.removePersistentDomain(forName: domain)
-        //        UserDefaults.standard.synchronize()
+        let debug = false
+        if debug {
+            let domain = Bundle.main.bundleIdentifier!
+            UserDefaults.standard.removePersistentDomain(forName: domain)
+            UserDefaults.standard.synchronize()
+        }
         
-        let x = (Int)(self.width / 2) - 100
-        let y = (Int)(self.height) - 200
-        let button = NSButton(frame: CGRect(x: x, y: y, width: 200, height: 60))
-        layer = CALayer()
-        let text = CATextLayer()
-        layer.backgroundColor = Colors.Green.getColor().cgColor
-        layer.cornerRadius = 30
-        text.string = "Home"
-        text.frame = CGRect(x: 0, y: button.bounds.height / 3.2, width: button.bounds.width, height: button.bounds.height / 2)
-        text.foregroundColor = NSColor.darkGray.cgColor
-        text.fontSize = 16
-        text.alignmentMode = .center
-        layer.addSublayer(text)
-        button.layer = layer
-        button.action = #selector(goToHome)
-        button.setBorder(width: 3, color: .darkGray)
-        self.view.addSubview(button)
+        setupButtonView(button: homeView, title: "Home", color: .Green, action: #selector(goToHome))
         
-        let tWidth = (Int)(self.width) - 40
-        let tHeight = (Int)(self.height) - 300
-        table = NSTableView(frame: CGRect(x: 20, y: 60, width: tWidth, height: tHeight))
-//        table!.register(NSTableViewCell.self, forCellReuseIdentifier: "Cell")
-        table!.dataSource = self
-        table!.delegate = self
-        self.view.addSubview(table!)
+        challengeView.dataSource = self
+        challengeView.delegate = self
+        challengeView.target = self
+        challengeView.doubleAction = #selector(doubleClick(_:))
         getJson()
+    }
+    
+    override func viewDidAppear() {
+        puzzleSelected = -1
+        challengeView.reloadData()
     }
     
     override func prepare(for segue: NSStoryboardSegue, sender: Any?) {
